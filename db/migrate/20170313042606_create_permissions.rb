80e2bcdecce850259cf8f0d7358caca162854e2a
Type in your name <Type in your email>
2017-03-13 11:47:26 +0700

    fix permission

--- db/migrate/20170313042606_create_permissions.rb
+++ db/migrate/20170313042606_create_permissions.rb
@@ -2,7 +2,8 @@ class CreatePermissions < ActiveRecord::Migration[5.0]
   def change
     create_table :permissions do |t|
       t.string :entry
-      t.hash :optional
+      t.text :optional
+      t.references :group, foreign_key: true
 
       t.timestamps
     end
