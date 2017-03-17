3a748def0aae17869bed4c322249e53d8e9be8cb
luuthomvp <luuthomvp@gmail.com>
2017-03-17 11:03:39 +0700

    form-login

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w(admin/admin.css admin/admin.js)
 Rails.application.config.assets
   .precompile += %w(template education.scss education.js)
+Rails.application.config.assets.precompile += %w(education.css)
