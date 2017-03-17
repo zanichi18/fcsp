f72df143e48b726d21ac91eb9c8969f6e3b5cdd1
zanichi18 <thanhtam12t3@gmail.com>
2017-03-17 12:42:51 +0700

    fix load css js education

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(template education/projects)
 Rails.application.config.assets.precompile += %w(admin/admin.css admin/admin.js)
+Rails.application.config.assets
+  .precompile += %w(template education.scss education.js)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   devise_for :users
   get "/pages/*page" => "pages#show"
   root "pages#home"
