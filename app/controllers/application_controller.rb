ab3c1ce3bc3e1f429f546b2fc288c2220908bf8a
luuthomvp <luuthomvp@gmail.com>
2017-03-20 09:10:50 +0700

    fix-form-login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,13 @@ class ApplicationController < ActionController::Base
   end
 
   private
-
   def rack_mini_profiler_authorize_request
     environments = Rails.application.config.rack_mini_profiler_environments
     return unless Rails.env.in? environments
     Rack::MiniProfiler.authorize_request
+  end
+
+  def after_sign_out_path_for _resource
+    root_path
   end
 end
