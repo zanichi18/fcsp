3e35b2a5d6ba4cdc9b7b64daf0706580b3bb2c9a
at-quoctv <thuyvietquoc1@gmail.com>
2017-03-21 15:54:36 +0700

    fix-instance-variable-reek

--- app/controllers/education/home_controller.rb
+++ app/controllers/education/home_controller.rb
@@ -1,13 +1,5 @@
 class Education::HomeController < Education::BaseController
   def index
-    @about = Education::About.first
-    @projects = Education::Project.newest.includes(:images)
-      .limit Settings.home.limit
-    @learning_programs = Education::LearningProgram
-      .limit Settings.education.index.max_learning_program
-    @trainings = Education::Training.newest.includes(:techniques)
-      .limit Settings.home_trainings_limit
-    @courses = Education::Course.newest
-      .limit Settings.courses.home_limit
+    @home_object = Supports::Education::Home.new
   end
 end
--- app/models/supports/education/home.rb
+++ app/models/supports/education/home.rb
@@ -1,9 +1,29 @@
 module Supports::Education
   class Home
-    attr_reader :project
+    attr_reader :about, :projects, :learning_programs, :trainings, :courses
 
-    def initialize project
-      @project = project
+    def about
+      Education::About.first
+    end
+
+    def projects
+      Education::Project.newest.includes(:images)
+        .limit Settings.home.limit
+    end
+
+    def learning_programs
+      Education::LearningProgram
+        .limit Settings.education.index.max_learning_program
+    end
+
+    def trainings
+      Education::Training.newest.includes(:techniques)
+        .limit Settings.home_trainings_limit
+    end
+
+    def courses
+      Education::Course.newest
+        .limit Settings.courses.home_limit
     end
   end
 end
