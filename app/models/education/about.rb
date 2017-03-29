db6f600c8b2046d58198715451b7e4c30057219f
lequangcanh <lequangcanh94@gmail.com>
2017-03-29 17:13:15 +0700

    admin update about us

--- app/models/education/about.rb
+++ app/models/education/about.rb
@@ -1,5 +1,10 @@
 class Education::About < ApplicationRecord
   translates :title, :content
 
-  has_many :images, class_name: Education::Image.name, as: :imageable
+  has_one :image, class_name: Education::Image.name, as: :imageable
+
+  validates :title, presence: true
+  validates :content, presence: true
+
+  accepts_nested_attributes_for :image, allow_destroy: true
 end
