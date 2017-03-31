99ae0788082b7d95271b2f61b1198a6088fd5d1e
at-quoctv <thuyvietquoc1@gmail.com>
2017-03-31 15:18:39 +0700

    fix-n+1-for-trainings-post

--- app/models/supports/education/index_post.rb
+++ app/models/supports/education/index_post.rb
@@ -19,7 +19,7 @@ module Supports::Education
 
     def posts
       search = Education::Post.search(content_cont: @param_q)
-      posts = search.result distinct: true
+      posts = search.result(distinct: true).includes :user, :category
       posts = posts.by_user @param_user if @param_user.present?
       posts = posts.by_category_id @param_category if @param_category.present?
       posts.created_desc.page(@param_page)
