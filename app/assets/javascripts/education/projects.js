12101b4855b99709f725890db2d5cd1fd66b6344
tran anh vu <eyelight93@gmail.com>
2017-03-31 09:11:35 +0700

    fix conflict image form input

--- app/assets/javascripts/education/projects.js
+++ app/assets/javascripts/education/projects.js
@@ -43,6 +43,26 @@ $(document).ready(function() {
       delete_project(id);
     }
   })
+
+  var onAddFile;
+  onAddFile = function(event) {
+    var file, thumbContainer, url;
+    file = event.target.files[0];
+    url = URL.createObjectURL(file);
+    thumbContainer = $(this).parent().parent();
+    if (thumbContainer.find('img').length === 0) {
+      return thumbContainer.append('<img src="' + url + '" />');
+    } else {
+      return thumbContainer.find('img').attr('src', url);
+    }
+  };
+  $('input[type=file]').each(function() {
+    return $(this).change(onAddFile);
+  });
+  $('body').on('cocoon:after-insert', function(e, addedPartial) {
+    return $('input[type=file]', addedPartial).change(onAddFile);
+  });
+  $('a.add_fields').data('association-insertion-method', 'append');
 });
 
 function delete_project(id) {
