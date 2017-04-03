523e5435d0afcde4c52a882067e2081be91f9594
Kominam <kominam.2511@gmail.com>
2017-04-03 13:47:30 +0700

    Display job in homepage

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe PagesController, type: :controller do
+  let(:job){FactoryGirl.create :job}
+  describe "GET #index" do
+    it "populates an array of jobs" do
+      get :index
+      expect(assigns(:jobs)).to include job
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      expect(response).to be_success
+      expect(response).to have_http_status 200
+    end
+  end
+end
