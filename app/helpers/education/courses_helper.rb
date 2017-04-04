db08986de3f1663d76da50ec1f6d5f3e002ee863
Pham Van Chien <phamvanbk11@gmail.com>
2017-04-04 17:10:22 +0700

    fix cut size

--- app/helpers/education/courses_helper.rb
+++ app/helpers/education/courses_helper.rb
@@ -2,4 +2,8 @@ module Education::CoursesHelper
   def training_select
     Education::Training.with_deleted.all.map{|training| [training.name, training.id]}
   end
+
+  def check_size_max?
+    training_select.size > Settings.courses.number_filter
+  end
 end
