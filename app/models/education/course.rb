3b6ec51619e5921365d13e64e976cdf8abd91698
at-quoctv <thuyvietquoc1@gmail.com>
2017-04-04 14:56:22 +0700

    soft-delete-course

--- app/models/education/course.rb
+++ app/models/education/course.rb
@@ -1,7 +1,7 @@
 class Education::Course < ApplicationRecord
   translates :detail
 
-  belongs_to :training, class_name: Education::Training.name
+  belongs_to :training, -> { with_deleted }, class_name: Education::Training.name
 
   has_many :course_members, class_name: Education::CourseMember.name,
     foreign_key: :course_id, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329081130) do
+ActiveRecord::Schema.define(version: 20170404071857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -352,6 +352,8 @@ ActiveRecord::Schema.define(version: 20170329081130) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_education_trainings_on_deleted_at", using: :btree
   end
 
   create_table "education_user_groups", force: :cascade do |t|
