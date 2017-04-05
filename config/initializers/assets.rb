ff7daeeebe5c35902a54343f1786ad08dda10640
Tran Xuan Nam <tran.xuan.nam@framgia.com>
2017-04-05 18:31:45 +0700

    add_member_to_group

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(admin/admin.css admin/admin.js
   template education.scss education/project_search education/management/groups
-  education.js employer.scss employer.js education/trainings.js education/projects.js education/posts_search.js education/management/feedbacks)
+  education.js employer.scss employer.js education/trainings.js
+  education/projects.js education/posts_search.js education/management/feedbacks
+  education/management/group_users)
