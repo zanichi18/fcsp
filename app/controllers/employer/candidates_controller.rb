a9906a964c59c94b2d343db56c95d42cf53bc5b6
nghiantt <nguyen.thi.trong.nghia@framgia.com>
2017-04-05 17:05:47 +0700

    fix show job

--- app/controllers/employer/candidates_controller.rb
+++ app/controllers/employer/candidates_controller.rb
@@ -2,8 +2,8 @@ class Employer::CandidatesController < Employer::BaseController
   load_resource :company
 
   def index
-    params[:job_id] = @company.jobs.pluck(:id) if (params[:job_id].nil? ||
-      params[:job_id].empty?)
+    params[:job_id] = @company.jobs.pluck(:id) if params[:job_id].nil? ||
+      params[:job_id].empty?
     @object = Supports::Candidate.new @company, params[:job_id]
     respond_to do |format|
       if request.xhr?
