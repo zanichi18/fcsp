0b65b91afa0ecdbaf6da3b7235c7885152494a86
Le Duc Tien <leductienttkt@gmail.com>
2017-04-19 14:50:37 +0700

    fix show activity

--- app/controllers/education/users_controller.rb
+++ app/controllers/education/users_controller.rb
@@ -2,8 +2,13 @@ class Education::UsersController < Education::BaseController
   load_resource
 
   def show
-    @show_user_object = Supports::Education::ShowUser.new @user, params[:page]
-    @activities = Activity.load_by_current_user(current_user).recent
-      .pagination params[:page]
+    if @user.education_status_blocked?
+      flash[:danger] = t ".blocked"
+      redirect_to root_url
+    else
+      @show_user_object = Supports::Education::ShowUser.new @user, params[:page]
+      @activities = Activity.load_by_current_user(@user).recent
+        .pagination params[:page]
+    end
   end
 end
