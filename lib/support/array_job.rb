9b1e478f2e040489e472e50629304aa243bf9fcf
Pham Van Chien <phamvanbk11@gmail.com>
2017-04-25 11:22:12 +0700

    fix array job

--- lib/support/array_job.rb
+++ lib/support/array_job.rb
@@ -1,11 +1,9 @@
 class ArrayJob
   class << self
     def get_job job_object, user_object
-      arr_job = Array.new
       job_object.select do |job|
-        arr_job << job unless (user_object.skills & job.skills).empty?
+        (user_object.skills & job.skills).present?
       end
-      arr_job
     end
   end
 end
